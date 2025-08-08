c31114ba919b51ae21a0d0b22c803b1dde2f9c84
Arun Deva <arundevapython63@gmail.com>
2025-08-08 13:39:43 +0100

    removed unwanted fl

--- temp_task2/backend.tf
+++
@@ -1,9 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "terraform-state-ecs-demo-arun-20250731"
-#     key            = "terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform-locks"
-#     encrypt        = true
-#   }
-# }
